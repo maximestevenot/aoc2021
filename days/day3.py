f36180057ede92bbc38d449828e9ea6c0ee64ba1
STEVENOT Maxime <maxime.stevenot@soprabanking.com>
2021-12-03 10:51:51 +0100

    day 3 part 2 (dirty)

--- days/day3.py
+++ days/day3.py
@@ -1,5 +1,9 @@
 from common.day import Day
 
+ZERO = "0"
+
+ONE = "1"
+
 
 class Day3(Day):
 
@@ -25,15 +29,54 @@ class Day3(Day):
     def count_bits(table):
         result = []
         for i in range(len(table[0])):
-            one = 0
-            zero = 0
-            for line in table:
-                if line[i] == "0":
-                    zero += 1
-                else:
-                    one += 1
-            result.append((one, zero))
+            result.append(Day3.count_one_zero_col(table, i))
         return result
 
+    @staticmethod
+    def count_one_zero_col(table: list[str], col_index: int):
+        one = 0
+        zero = 0
+        for line in table:
+            if line[col_index] == "0":
+                zero += 1
+            else:
+                one += 1
+        return one, zero
+
+    @staticmethod
+    def get_max_bit(r: (int, int)):
+        return ONE if max(r) == r[0] else ZERO
+
+    @staticmethod
+    def get_min_bit(r: (int, int)):
+        return ZERO if max(r) == r[0] else ONE
+
+
     def part2(self) -> int:
-        return -9999999999
+        table = self.read_input_lines()
+        # result = self.count_bits(table)
+        new_table = self.filter_data_oxygen(0, table, len(table[0]))
+
+        oxygen = int("".join(new_table), 2)
+
+        table2 = self.filter_data_co2(0, table, len(table[0]))
+        co2 = int("".join(table2), 2)
+
+        return co2 * oxygen
+
+    def filter_data_oxygen(self, i, table, stop):
+        if i < stop and len(table) > 1:
+            r = self.count_one_zero_col(table, i)
+            new_table = list(filter(lambda x: x[i] == self.get_max_bit(r), table))
+            return self.filter_data_oxygen(i+1, new_table[0:max(r)], stop)
+        return table
+
+    def filter_data_co2(self, i, table, stop):
+        print(i)
+        if i < stop and len(table) > 1:
+            r = self.count_one_zero_col(table, i)
+            print(r)
+            new_table = list(filter(lambda x: x[i] == self.get_min_bit(r), table))
+            print(new_table)
+            return self.filter_data_co2(i+1, new_table[0:min(r)], stop)
+        return table
