5154ad19e5eda8b5f59216a41f45271a4f2e7396
STEVENOT Maxime <maxime.stevenot@soprabanking.com>
2021-12-07 18:32:52 +0100

    feat: day 7

--- common/day_factory.py
+++ common/day_factory.py
@@ -4,6 +4,7 @@ from days.day2 import Day2
 from days.day3 import Day3
 from days.day4 import Day4
 from days.day5 import Day5
+from days.day7 import Day7
 
 
 class UnimplementedDayException(Exception):
@@ -27,5 +28,7 @@ class DayFactory:
             return Day4(default_file_name(index, example))
         if index == 5:
             return Day5(default_file_name(index, example))
+        if index == 7:
+            return Day7(default_file_name(index, example))
         else:
             raise UnimplementedDayException(f'Day {index} not implemented')
---
+++ days/day7.py
@@ -0,0 +1,34 @@
+from common.day import Day
+
+
+def compute_fuel_consumption_part_1(position: int, target_position: int) -> int:
+    return abs(position - target_position)
+
+
+def compute_fuel_consumption_part_2(position: int, target_position: int) -> int:
+    return sum(range(1, abs(position - target_position) + 1))
+
+
+class Day7(Day):
+
+    def part1(self) -> int:
+        return self.compute_min_fuel(compute_fuel_consumption_part_1)
+
+    def part2(self) -> int:
+        return self.compute_min_fuel(compute_fuel_consumption_part_2)
+
+    def compute_min_fuel(self, compute_fuel_consumption) -> int:
+        positions = [int(x) for x in self.read_input_lines()[0].split(',')]
+        self.logger.debug(positions)
+
+        target_positions = list(range(min(positions), max(positions)))
+        fuel_list = []
+        for tp in target_positions:
+            fuel = 0
+            for p in positions:
+                fuel += compute_fuel_consumption(p, tp)
+            fuel_list.append((tp, fuel))
+
+        min_fuel = min(fuel_list, key=lambda x: x[1])
+        self.logger.debug(min_fuel)
+        return min_fuel[1]
